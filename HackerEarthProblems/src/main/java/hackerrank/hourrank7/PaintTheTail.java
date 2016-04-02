ea49ce1015770b88bd753b9980f40341eafa6ab8
Akhilesh Kumar Maurya <maurya_akhilesh@ymail.com>
2016-04-02 21:50:44 +0530

    hackerHour7

---
+++ HackerEarthProblems/src/main/java/hackerrank/hourrank7/PaintTheTail.java
@@ -0,0 +1,23 @@
+package hackerrank.hourrank7;
+
+import java.util.Scanner;
+
+public class PaintTheTail {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		String C = in.next();
+		char ch=C.charAt(0);
+		int count=1;
+		for (int i = 1; i < N; i++) {
+			if(ch!=C.charAt(i)){
+				count++;
+				ch=C.charAt(i);
+			}
+		}
+		System.out.println(count);
+
+	}
+
+}
